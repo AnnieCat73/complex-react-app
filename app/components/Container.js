fbc25873775814356827b65ccfa8598ab835f541
Annie <devannie777@gmail.com>
2022-04-02 12:46:23 +0100

    Page component

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react"
 
+//So pass this component Container via props and they will inherit the following CSS 
+//classes. If props.wide is false will also have container--narrow class
+
 function Container(props) {
   return (
     <div className={"container py-md-5 " + (props.wide ? '' : 'container--narrow')}>
